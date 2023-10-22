5f46d9cccdece208403fe9f7cc42fc199ae70d51
Frank <fpatino@fi.uba.ar>
2023-10-22 19:21:41 -0300

    fix delete en cascada de account

--- src/main/java/com/aninfo/model/Transaction.java
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -1,5 +1,8 @@
 package com.aninfo.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -11,6 +14,7 @@ public class Transaction {
     private Long id;
 
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Account account;
 
     private Double amount;
